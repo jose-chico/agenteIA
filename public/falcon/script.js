9b6c204dee9fb74e7c6a290ab8d9b610df93270e
jose-chico <franciscoclassname@gmail.com>
2026-03-14 17:27:42 -0300

    pagessss

--- public/falcon/script.js
+++ public/falcon/script.js
@@ -187,15 +187,15 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
         e.preventDefault();
 
-        const target = document.querySelector(this.getAttribute("href"));
-        if (target) {
-            target.scrollIntoView({
-                behavior: "smooth",
-                block: "start"
-            });
-        }
+            const target = document.querySelector(this.getAttribute("href"));
+            if (target) {
+                target.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
+                });
+            }
+        });
     });
-});
 
 // Botão Hero
 const heroButton = document.querySelector(".hero .btn");
